010d1ea6e8233cd310cb193012d3d37fd65b8c5f
sergio <sbpss@iscte-iul.pt>
2018-05-05 15:26:07 +0100

    Pom File

--- SensorMongo/src/SensorListener.java
+++ SensorMongo/src/SensorListener.java
@@ -1,4 +1,9 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class SensorListener {
-
+	
 }
